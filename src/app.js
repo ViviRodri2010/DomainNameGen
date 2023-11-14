76b30fdd75e55accf86dfe6eb82591d97fbfb70f
ViviRodri2010 <146996438+ViviRodri2010@users.noreply.github.com>
2023-11-14 03:18:34 +0000

    Created script code

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big", "amazing"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io", ".es", ".dev"];
+
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let extension of extensions) {
+          console.log(pronoun + adjective + noun + extension);
+        }
+      }
+    }
+  }
 };
